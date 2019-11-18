0f3217d21cce23d3c61a6e4dce202e5a14bb75d7
Prateek Senapati <50922445+prateek-senapati@users.noreply.github.com>
2019-11-18 11:57:42 +0530

    Update Schedule.java

--- src/Schedule.java
+++ src/Schedule.java
@@ -16,44 +16,49 @@ class Schedule implements Serializable {
         black = white = blackID = whiteID = result = "TBD";
     }
 
+    // method to store the result of the current match
     private void updateMatchDetails () {
 
         Scanner input = new Scanner(System.in);
-        System.out.print("\nWho won the match? (black/white/draw) or (b/w/d): ");
+        System.out.print("\n\nWho won the match? (black/white/draw) or (b/w/d): ");
         result = input.nextLine();
     }
 
+    // method to print a particular match's details
     private void getMatchDetails () {
 
-        System.out.println("\nMatch Number: " + matchNumber);
-        System.out.println("Black: " + black);
-        System.out.println("Black ID: " + blackID);
-        System.out.println("White: " + white);
-        System.out.println("White ID: " + whiteID);
-        System.out.println("Result: " + result);
+        System.out.println();
+        System.out.print(String.format("%15s", matchNumber));
+        System.out.print(String.format("%15s", blackID));
+        System.out.print(String.format("%25s", black));
+        System.out.print(String.format("%25s", white));
+        System.out.print(String.format("%15s", whiteID));
+        System.out.print(String.format("%10s", result));
     }
 
+    // method to save the fixtures in a file
     private void saveSchedule () throws IOException {
 
-        FileOutputStream FOS = new FileOutputStream("schedule-6.txt");
+        FileOutputStream FOS = new FileOutputStream("schedule-11.txt");
         ObjectOutputStream OOS = new ObjectOutputStream(FOS);
         OOS.writeObject(schedule);
         OOS.close();
     }
 
+    // method to generate the fixtures
     int generateSchedule () throws IOException, ClassNotFoundException {
 
-        File file = new File("chess-6.txt");
+        File file = new File("chess-11.txt");
         if (file.exists())
         {
-            FileInputStream FIS = new FileInputStream("chess-6.txt");
+            FileInputStream FIS = new FileInputStream("chess-11.txt");
             ObjectInputStream OIS = new ObjectInputStream(FIS);
             @SuppressWarnings("unchecked")
             ArrayList<ChessPlayer> playersOutput = (ArrayList<ChessPlayer>) OIS.readObject();
+            // shuffles the list of players to maintain randomness in the selection of black/white players
             Collections.shuffle(playersOutput);
             int n = playersOutput.size();
-            // your code here
-
+            // the following for loop generates the fixtures
             for (int i = 0; i < playersOutput.size() - 1; ++i) {
                 for (int j = i + 1; j < playersOutput.size(); ++j) {
                     Schedule item = new Schedule();
@@ -64,24 +69,35 @@ class Schedule implements Serializable {
                     schedule.add(item);
                 }
             }
+            // shuffles the list of fixtures to maintain randomness in the schedule
             Collections.shuffle(schedule);
             for (int i = 0; i < schedule.size(); ++i)
-                schedule.get(i).matchNumber = i + 1;
+                schedule.get(i).matchNumber = i + 1; // sets match number for the generated schedule
             saveSchedule();
-            return (n * (n - 1)) / 2;
+            return (n * (n - 1)) / 2; // returns the total number of matches to be played
         }
         else {
-            System.out.println("\nFile does not exist. Please add a player first.");
+            System.out.println("\n\nFile does not exist. Please add a player first.");
             return 0;
         }
     }
 
+    // method to print the complete schedule
     void showSchedule () throws IOException, ClassNotFoundException {
 
-        File file = new File("schedule-6.txt");
+        File file = new File("schedule-11.txt");
         if (file.exists())
         {
-            FileInputStream FIS = new FileInputStream("schedule-6.txt");
+            System.out.println();
+            System.out.println();
+            System.out.print(String.format("%15s", "Match No"));
+            System.out.print(String.format("%15s", "Black ID"));
+            System.out.print(String.format("%25s", "Black"));
+            System.out.print(String.format("%25s", "White"));
+            System.out.print(String.format("%15s", "White ID"));
+            System.out.print(String.format("%10s", "Result"));
+            System.out.println();
+            FileInputStream FIS = new FileInputStream("schedule-11.txt");
             ObjectInputStream OIS = new ObjectInputStream(FIS);
             @SuppressWarnings("unchecked")
             ArrayList<Schedule> scheduleOutput = (ArrayList<Schedule>) OIS.readObject();
@@ -90,34 +106,37 @@ class Schedule implements Serializable {
             OIS.close();
         }
         else
-            System.out.println("\nFile does not exist. Please add a player first.");
+            System.out.println("\n\nFile does not exist. Please add a player first.");
     }
 
+    // method to update the details of the current match
     void updateSchedule (int matchNumber) throws IOException, ClassNotFoundException {
 
-        File file = new File("schedule-6.txt");
+        File file = new File("schedule-11.txt");
         if (file.exists())
         {
-                FileInputStream FIS = new FileInputStream("schedule-6.txt");
+                FileInputStream FIS = new FileInputStream("schedule-11.txt");
                 ObjectInputStream OIS = new ObjectInputStream(FIS);
                 @SuppressWarnings("unchecked")
                 ArrayList<Schedule> scheduleOutput = (ArrayList<Schedule>) OIS.readObject();
                 schedule = new ArrayList<>(scheduleOutput);
                 if (schedule.isEmpty()) {
-                    System.out.println("No player has been registered yet. Please add a player first.");
+                    System.out.println("\n\nNo player has been registered yet. Please add a player first.");
                     return;
                 }
-                if (matchNumber < 1 || matchNumber > schedule.size()) {
-                    System.out.println("\nInvalid match number entered.");
+                // checks the validity of the match number
+                if (matchNumber < 1 || matchNumber > schedule.size()) { 
+                    System.out.println("\n\nInvalid match number entered.");
                     return;
                 }
                 schedule.get(matchNumber - 1).updateMatchDetails();
                 saveSchedule();
                 Table t = new Table();
-                t.updateTable(schedule.get(matchNumber - 1));
+                // updates the points table based on the current match's result
+                t.updateTable(schedule.get(matchNumber - 1)); 
                 OIS.close();
         }
         else
-            System.out.println("\nFile does not exist. Please add a player first.");
+            System.out.println("\n\nFile does not exist. Please add a player first.");
     }
 }
